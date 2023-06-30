f8409cffdc64a12c6a9dc1e6c2c42bd592f62cad
martin.dubois <martin.dubois@foncia.com>
2023-06-30 11:19:39 +0200

    feat: update Factory for recipe card

--- scripts/factories/recipeCardFactory.js
+++ scripts/factories/recipeCardFactory.js
@@ -1,23 +1,84 @@
-const getRecipeCardDom = data => {
-
+const getRecipeCardDom = recipes => {
   // eslint-disable-next-line no-unused-vars
-  const { image, appliance, description, id, ingredients, name, servings, time, ustensils } = data;
+  const { image, appliance, description, id, ingredients, name, servings, time, ustensils } = recipes;
 
-  const picture = `images/${image}`;
-  console.log(picture);
+  console.log(recipes);
+  const photoPath = `images/${image}`;
 
   const article = document.createElement('article');
+  const recipeContentDiv = document.createElement('div');
+  const divPhoto = document.createElement('div');
+  const divTime = document.createElement('div');
+  const divRecipe = document.createElement('div');
+  const divIngredients = document.createElement('div');
+
   article.className = "recipe-article";
-  const img = document.createElement('img');
-  img.setAttribute("src", picture);
-  img.setAttribute("alt", name);
-  img.className = "recipe-photo";
-  const h2 = document.createElement('h2');
-  h2.textContent = name;
-  const p = document.createElement('p');
-  p.textContent = description;
-  article.appendChild(img);
-  article.appendChild(h2);
+  recipeContentDiv.className = "recipe-content";
+  divPhoto.className = "container__recipe-photo";
+  divTime.className = "container__recipe-time";
+  divRecipe.className = "container__recipe-text";
+  divIngredients.className = "container__recipe-ingredients";
+
+  const recipePhoto = document.createElement('img');
+  recipePhoto.setAttribute("src", photoPath);
+  recipePhoto.setAttribute("alt", name);
+  recipePhoto.className = "recipe-photo";
+
+  const recipeTitle = document.createElement('h3');
+  recipeTitle.className = "recipe-title";
+  recipeTitle.textContent = name;
+
+  const recipeTime = document.createElement('p');
+  recipeTime.className = "recipe-time";
+  recipeTime.textContent = `${time} min`;
+
+  const recipeTextTitle = document.createElement('h4');
+  recipeTextTitle.className = "recipe-mini-title";
+  recipeTextTitle.textContent = "RECETTE";
+
+  const recipeText = document.createElement('p');
+  recipeText.className = "recipe-description";
+  recipeText.textContent = description;
+
+  const recipeIngredientsTitle = document.createElement('h4');
+  recipeIngredientsTitle.className = "recipe-mini-title";
+  recipeIngredientsTitle.textContent = "INGRÉDIENTS";
+
+  article.appendChild(divPhoto);
+  article.appendChild(divTime);
+  article.appendChild(recipeContentDiv);
+  recipeContentDiv.appendChild(recipeTitle);
+  recipeContentDiv.appendChild(divRecipe);
+  recipeContentDiv.appendChild(divIngredients);
+
+  divPhoto.appendChild(recipePhoto);
+  divRecipe.appendChild(recipeTextTitle);
+  divRecipe.appendChild(recipeText);
+  divIngredients.appendChild(recipeIngredientsTitle);
+  divTime.appendChild(recipeTime);
+
+  ingredients.forEach((ingredient) => {
+
+    const divIngredient = document.createElement('div');
+    divIngredients.appendChild(divIngredient);
+    divIngredient.setAttribute("class","div-ingredient");
+
+    const ingredientRecette = document.createElement('p');
+    divIngredient.appendChild(ingredientRecette);
+    ingredientRecette.setAttribute("class","ingredient");
+    ingredientRecette.textContent = ingredient.ingredient;
+
+    const quantity = document.createElement('p');
+    divIngredient.appendChild(quantity);
+    quantity.setAttribute("class","quantity-unit");
+    quantity.textContent = ingredient.quantity;
+
+    const unit = document.createElement('p');
+    divIngredient.appendChild(unit);
+    unit.setAttribute("class","quantity");
+    unit.textContent = ingredient.unit;
+
+  });
   return (article);
 };
 
